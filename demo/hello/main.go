81c22466ae9bd5ebc8b9991f3a52051efb07237b
Tjeerd Hes <tjeerd.hes@icloud.com>
2023-04-09 19:26:43 +0200

    [demo][hello] create table test

--- demo/hello/main.go
+++ demo/hello/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func hello(sr *string) (result string) {
@@ -14,12 +15,26 @@ func hello(sr *string) (result string) {
 	return
 }
 
+func helloInt(n int) (result string) {
+	result = fmt.Sprintf("hello number %d", n)
+	return
+}
+
+func helloLoop(args []string) {
+	for _, a := range args {
+		n, err := strconv.Atoi(a)
+		if err != nil {
+			fmt.Println(hello(&a))
+		} else {
+			fmt.Println(helloInt(n))
+		}
+	}
+}
+
 func main() {
 	args := os.Args
 	if len(args) > 1 {
-		for _, a := range args[1:] {
-			fmt.Println(hello(&a))
-		}
+		helloLoop(args[1:])
 	} else {
 		fmt.Println(hello(nil))
 	}
