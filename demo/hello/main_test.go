6c1b3a512a7ad0b6d1951497f6cfbcdbd7dd1cda
Tjeerd Hes <tjeerd.hes@icloud.com>
2023-04-09 19:26:43 +0200

    [demo][hello] create table test

--- demo/hello/main_test.go
+++ demo/hello/main_test.go
@@ -24,4 +24,31 @@ func Test_hello(t *testing.T) {
 		t.Errorf("failed, expect: %s", expect)
 	}
 
+	eleven := 11
+	expect = fmt.Sprintf("hello number %d", eleven)
+	if helloInt(eleven) != expect {
+		t.Errorf("failed, expect: %s", expect)
+	}
+
+}
+
+func Test_hello_with_table(t *testing.T) {
+	helloEntries := []struct {
+		name            string
+		values          []string
+		expectedResults []string
+		msg             string
+	}{
+		{name: "first test", values: []string{"Bob", ""}, expectedResults: []string{"hello Bob", "hello world"}, msg: "first hello test(s)"},
+		{name: "next test", values: []string{"there"}, expectedResults: []string{"hello there"}, msg: "next hello test"},
+	}
+
+	for _, e := range helloEntries {
+		for i, a := range e.values {
+			r := hello(&a)
+			if r != e.expectedResults[i] {
+				t.Errorf("failed, %s; result: %s != expected: %s", e.msg, r, e.expectedResults[i])
+			}
+		}
+	}
 }
